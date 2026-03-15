b2765658ff057373fce5c0215737d72e9388b9df
nedirjasmine302-bit <nedirjasmine302@gmail.com>
2026-03-15 18:14:17 +0100

    Modif Router

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -1,8 +1,7 @@
 import Route from "./Route.js";
-
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),];
+  new Route("/", "Accueil", "/pages/home.html"),];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "FantasyRealm Online";

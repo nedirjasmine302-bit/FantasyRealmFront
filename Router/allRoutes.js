b2f880f49ec759ef1f15f1658e696ae6d765d63f
nedirjasmine302-bit <nedirjasmine302@gmail.com>
2026-06-04 16:51:01 +0200

    Page favoris complete (html+css+js)

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,6 +3,7 @@ import Route from "./Route.js";
 export const allRoutes = [
   new Route("/", "Accueil", "/pages/home.html","/assets/js/src/home.js"),
   new Route("/character", "Personnages", "/pages/character.html","/assets/js/src/character.js"),
-  new Route("/my_space", "Mon Espace", "/pages/my_space.html","/assets/js/src/my_space.js")];
+  new Route("/my_space", "Mon Espace", "/pages/my_space.html","/assets/js/src/my_space.js"),
+  new Route("/favoris", "Favoris", "/pages/favoris.html","/assets/js/src/favoris.js")];
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "FantasyRealm Online";
